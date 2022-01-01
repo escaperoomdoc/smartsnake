4216c3323f9ed2d9b287419aeeaaa39895f6f347
escaperoomdoc <escaperoomdoc@gmail.com>
2022-01-01 22:44:28 +0300

    +

--- main.py
+++ main.py
@@ -5,12 +5,41 @@ cell_size = 15
 cells = 40
 BG_COLOR = (0, 0, 0)
 resolution = (cell_size * cells, cell_size * cells)
-snakes = [Snake(cells)]
+
+class Generation:
+	def __init__(self, population=100, cells=40):
+		self.population = population
+		self.cells = cells
+		self.new()
+	
+	def step(self):
+		snakes_alive = 0
+		for snake in self.snakes:
+			if not snake.alive: continue
+			dir = snake.think()
+			snake.move(dir)
+			if snake.alive: snakes_alive += 1
+		if snakes_alive == 0: self.regenerate()
+	
+	def regenerate(self):
+		snakes = sorted(self.snakes, key=lambda x: x.fitness())
+		genes = [snakes[-1].i2h, snakes[-2].i2h, snakes[-1].h2o, snakes[-2].h2o]
+		snakes = None
+		self.new(genes)
+
+	def new(self, genes=None):
+		self.snakes = []
+		for i in range(self.population):
+			self.snakes.append(Snake(MAX=cells, size=5, genes=genes))
+		pass
+
+
+gen = Generation()
 
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption('snake')
 
-def drawRect(y: int, x: int, color):
+def draw_rect(y: int, x: int, color):
 	pygame.draw.rect(screen, color, (x * cell_size - 1, y * cell_size - 1, cell_size - 2, cell_size - 2))
 
 while True:
@@ -22,20 +51,30 @@ while True:
 			exit_flag = True
 		if event.type == pygame.KEYDOWN:
 			dir = None
+			step = False
 			if event.key == pygame.K_LEFT: dir = (0, -1)
 			elif event.key == pygame.K_RIGHT: dir = (0, 1)
 			elif event.key == pygame.K_UP: dir = (-1, 0)
 			elif event.key == pygame.K_DOWN: dir = (1, 0)
+			elif event.key == pygame.K_SPACE: step = True
 			if dir:
-				for snake in snakes:
-					snake.move(dir)
+				for snake in gen.snakes:
+					if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+						wall = snake.scan(dir, Snake.Wall)
+						body = snake.scan(dir, Snake.Body)
+						food = snake.scan(dir, Snake.Food)
+						print(f'wall={wall}, body={body}, food={food}')
+					else:
+						snake.move(dir)
+			if step: gen.step()
 	if exit_flag: break
 	# draw snakes
 	screen.fill(BG_COLOR)
-	for snake in snakes:
+	for snake in gen.snakes:
+		if not snake.alive: continue
 		for body in snake.body:
-			drawRect(body[0], body[1], (196, 196, 196))
-		drawRect(snake.food[0], snake.food[1], (255, 0, 0))
+			draw_rect(body[0], body[1], (196, 196, 196))
+		draw_rect(snake.food[0], snake.food[1], (255, 0, 0))
 	# flip buffers
 	pygame.display.flip()
 
--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
-#import numpy as np
-#import pandas as pd
+import numpy as np
 import random
 random.seed()
 
@@ -12,17 +11,41 @@ class Snake:
 	UpLeft = (-1, -1)
 	DownRight = (1, 1)
 	DownLeft = (1, -1)
+	DIRS = (Up, Right, Down, Left, UpRight, UpLeft, DownRight, DownLeft)
 	Wall = 0
 	Body = 1
 	Food = 2
-	def __init__(self, MAX=40, gene=None):
+	def __init__(self, MAX=40, size=5, genes=None):
 		self.MAX = MAX
-		self.body = [(random.randint(0, self.MAX-1), random.randint(0, self.MAX-3))]
-		self.body.append((self.body[-1][0], self.body[-1][1] + 1))
-		self.body.append((self.body[-1][0], self.body[-1][1] + 1))
+		self.body = [(random.randint(0, self.MAX-1), random.randint(0, self.MAX-1-size))]
+		for i in range(size-1):
+			self.body.append((self.body[-1][0], self.body[-1][1] + 1))
 		self.food = None
 		self.genfood()
+		self.i2h = None
+		self.h2o = None
+		self.moves = 0
+		self.foods = 0
+		self.alive = True
+		if genes:
+			self.genesis(genes)
+		else:
+			self.i2h = np.random.rand(12, 24)
+			self.h2o = np.random.rand(4, 12)
 	
+	def genesis(self, genes):
+		self.i2h = np.zeros((12, 24))
+		self.h2o = np.zeros((4, 12))
+		for y in range(12):
+			for x in range(24):
+				self.i2h[y][x] = genes[0][y][x] if random.random() < 0.5 else genes[1][y][x]
+				if random.random() < 0.02: self.i2h[y][x] = random.random()
+		for y in range(4):
+			for x in range(12):
+				self.h2o[y][x] = genes[2][y][x] if random.random() < 0.5 else genes[3][y][x]
+				if random.random() < 0.02: self.h2o[y][x] = random.random()
+		pass
+
 	def genfood(self):
 		while not self.food or self.food in self.body:
 			self.food = (random.randint(0, self.MAX-1), random.randint(0, self.MAX-1))
@@ -33,12 +56,12 @@ class Snake:
 		for i in range(self.MAX):
 			count += 1
 			candidate = (candidate[0] + dir[0], candidate[1] + dir[1])
-			if target == Wall:
+			if target == Snake.Wall:
 				if (candidate[0] < 0 or candidate[1] < 0 or
 					candidate[0] >= self.MAX or candidate[1] >= self.MAX): break
-			elif target == Body:
-				if candidate in body: break
-			elif target == Food:
+			elif target == Snake.Body:
+				if candidate in self.body: break
+			elif target == Snake.Food:
 				if candidate == self.food: break
 			else: return 0
 		return (self.MAX - count) / self.MAX
@@ -48,11 +71,43 @@ class Snake:
 		candidate = (candidate[0] + dir[0], candidate[1] + dir[1])
 		if (candidate[0] < 0 or candidate[1] < 0 or
 			 candidate[0] >= self.MAX or candidate[1] >= self.MAX or
-			 candidate in self.body): return False
+			 candidate in self.body):
+			 self.alive = False
+			 return
 		self.body.append(candidate)
-		if candidate == self.food: self.genfood()
+		if candidate == self.food:
+			self.foods += 1
+			self.genfood()
 		else: self.body.pop(0)
-		return True
+		self.moves += 1
 
+	def sensors(self):
+		input = []
+		for dir in Snake.DIRS:
+			input.append(self.scan(dir, Snake.Wall))
+			input.append(self.scan(dir, Snake.Body))
+			input.append(self.scan(dir, Snake.Food))
+		return input
+
+	def relu(self, values):
+		for value in values:
+			value[0] = max(0.0, value[0])
+		return values
+	
 	def think(self):
-		pass
+		input = self.sensors()
+		input.append(1.0)
+		input_vector = np.array(input).reshape(25,1)
+		i2h = np.append(self.i2h, np.ones(12).reshape(12,1), axis=1)
+		sums = np.dot(i2h, input_vector)
+		hidden = self.relu(sums)
+		hidden_vector = np.vstack([hidden, [1.0]])
+		h2o = np.append(self.h2o, np.ones(4).reshape(4,1), axis=1)
+		outputs = np.dot(h2o, hidden_vector)
+		index = np.argmax(outputs)
+		if index < 0: index = 0
+		if index > 3: index = 3
+		return Snake.DIRS[index]
+	
+	def fitness(self):
+		return self.moves**2 * 2**min(self.foods, 10) * max(1, self.foods - 9)
